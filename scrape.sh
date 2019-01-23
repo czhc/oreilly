e469ef9a6b2b21a6877ec4311ad8f252bc1ba693
czhc <cedriczhchan@gmail.com>
2019-01-23 09:54:22 +0800

    misc:newline

--- scrape.sh
+++ scrape.sh
@@ -3,8 +3,13 @@
 [[ $# -eq 0 ]] && { echo "Usage: FILE=filename TOKEN=some-token [FORMAT=epub] [DIR=out] ./scrape.sh"; exit 1;}
 
 [[ -f ./env.sh ]] && source ./env.sh
+
 [[ -z $TOKEN ]] && echo "missing TOKEN" && exit 1
+
 [[ -z $FILE ]] && echo "missing FILE" && exit 1
+
 [[ -n $FORMAT ]] && FORMAT=$FORMAT || FORMAT=epub
+
 [[ -n $DIR ]] && DIR=$DIR || DIR=out
+
 wget -O $DIR/$FILE.$FORMAT https://www.oreilly.com/programming/free/files/$FILE.$FORMAT\?mkt_tok\=$TOKEN && exit 0
